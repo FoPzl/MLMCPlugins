cc699c843318abdefa1dd9e4b8fa50e6000d998c
FoPzl <officialsuperalex11@gmail.com>
2022-08-21 16:51:16 -0400

    Vote skele todo

---
+++ FoPzlVote/src/me/fopzl/vote/VoteListener.java
@@ -0,0 +1,13 @@
+package me.fopzl.vote;
+
+import com.vexsoftware.votifier.model.VotifierEvent;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class VoteListener implements Listener {
+	@EventHandler
+	public void onVote(final VotifierEvent event) {
+		
+	}
+}
