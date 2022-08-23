761ce4a3730b4f8dd31e99ff89f10997199abd1f
FoPzl <officialsuperalex11@gmail.com>
2022-08-23 05:16:13 -0400

    Some init command stuff

--- FoPzlVote/src/me/fopzl/vote/commands/MLVoteCommand.java
+++ FoPzlVote/src/me/fopzl/vote/commands/MLVoteCommand.java
@@ -1,20 +1,44 @@
 package me.fopzl.vote.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import me.fopzl.vote.Vote;
 
 public class MLVoteCommand implements CommandExecutor {
-
-	public MLVoteCommand(Vote vote) {
-		// TODO Auto-generated constructor stub
+	private Vote main;
+	
+	public MLVoteCommand(Vote main) {
+		this.main = main;
 	}
 
 	@Override
-	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
-		// TODO Auto-generated method stub
+	public boolean onCommand(CommandSender sender, Command cmd, String lbl, String[] args) {
+		if(args.length < 1) return false;
+		
+		if(sender.hasPermission("mlvote.admin")) {
+			switch(args[0]) {
+				case "vote":
+					if(args.length < 3) return false;
+					
+					main.rewardVote(Bukkit.getServer().getPlayer(args[1]));
+					// TODO: reqs specify this includes vote site, but how?
+					return true;
+				case "reload":
+					main.reloadConfig();
+					sender.sendMessage("§4[§c§lMLMC§4] §7Reloaded config");
+					return true;
+			}
+		}
+		
+		if (args[0].equalsIgnoreCase("leaderboard")) {
+			// TODO
+			return true;
+		}
+		
 		return false;
 	}
 
--- FoPzlVote/src/me/fopzl/vote/commands/VotePartyCommand.java
+++ FoPzlVote/src/me/fopzl/vote/commands/VotePartyCommand.java
@@ -1,5 +1,6 @@
 package me.fopzl.vote.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -7,14 +8,33 @@ import org.bukkit.command.CommandSender;
 import me.fopzl.vote.Vote;
 
 public class VotePartyCommand implements CommandExecutor {
+	private Vote main;
 
-	public VotePartyCommand(Vote vote) {
-		// TODO Auto-generated constructor stub
+	public VotePartyCommand(Vote main) {
+		this.main = main;
 	}
 
 	@Override
-	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
-		// TODO Auto-generated method stub
+	public boolean onCommand(CommandSender sender, Command cmd, String lbl, String[] args) {
+		if(args.length < 1) return false;
+		
+		if(sender.hasPermission("mlvote.admin")) {
+			switch(args[0]) {
+				case "add":
+					if(args.length < 2) return false;
+					// TODO: add to vp #
+					return true;
+				case "set":
+					if(args.length < 2) return false;
+					// TODO: set vp #
+					return true;
+			}
+		}
+		
+		if (args[0].equalsIgnoreCase("status")) {
+			// TODO
+		}
+		
 		return false;
 	}
 
