436e15b8769c86671ac6a1885470b064add7ba47
FoPzl <officialsuperalex11@gmail.com>
2022-08-23 04:58:02 -0400

    VoteListener core

--- FoPzlVote/src/me/fopzl/vote/Vote.java
+++ FoPzlVote/src/me/fopzl/vote/Vote.java
@@ -1,21 +1,37 @@
 package me.fopzl.vote;
 
+import java.util.HashSet;
+
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import me.fopzl.vote.commands.MLVoteCommand;
 import me.fopzl.vote.commands.VotePartyCommand;
 
 public class Vote extends JavaPlugin {
+	private HashSet<String> validVoteSites = new HashSet<String>();
+	
 	public void onEnable() {
+		super.onEnable();
 		Bukkit.getServer().getLogger().info("FoPzlVote Enabled");
 		
 		this.getCommand("mlvote").setExecutor(new MLVoteCommand(this));
 		this.getCommand("vp").setExecutor(new VotePartyCommand(this));
+		
+		getServer().getPluginManager().registerEvents(new VoteListener(this), this);
 	}
 	
 	public void onDisable() {
 		Bukkit.getServer().getLogger().info("FoPzlVote Disabled");
 		super.onDisable();
+	}
+	
+	public boolean isValidSite(String site) {
+		return validVoteSites.contains(site);
+	}
+	
+	public void rewardVote(Player p) {
+		
 	}
 }
--- FoPzlVote/src/me/fopzl/vote/VoteListener.java
+++ FoPzlVote/src/me/fopzl/vote/VoteListener.java
@@ -2,12 +2,49 @@ package me.fopzl.vote;
 
 import com.vexsoftware.votifier.model.VotifierEvent;
 
+import java.util.HashMap;
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class VoteListener implements Listener {
+	private Vote main;
+	private HashMap<UUID, Integer> queuedRewards = new HashMap<UUID, Integer>();
+	
+	public VoteListener(Vote main) {
+		this.main = main;
+	}
+	
+	@SuppressWarnings("deprecation")
 	@EventHandler
-	public void onVote(final VotifierEvent event) {
-		
+	public void onVote(final VotifierEvent e) {
+		if(main.isValidSite(e.getVote().getServiceName())) {
+			OfflinePlayer p = Bukkit.getServer().getOfflinePlayer(e.getVote().getUsername()); // TODO: verify case sensitivity
+			if(p.isOnline()) {
+				main.rewardVote((Player)p);
+			} else {
+				UUID uuid = p.getUniqueId();
+				queuedRewards.put(uuid, queuedRewards.getOrDefault(uuid, 0) + 1);
+			}
+			
+			// TODO: broadcast("&4[&c&lMLMC&4] &e" + playername + " &7just voted on &c" + servicename + "&7!")
+			// TODO: voteparty stuff
+		}
+	}
+	
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent e) {
+		UUID uuid = e.getPlayer().getUniqueId();
+		if(queuedRewards.containsKey(uuid)) {
+			int numRewards = queuedRewards.remove(uuid);
+			for(int i = 0; i < numRewards; i++) {
+				main.rewardVote(e.getPlayer());
+			}
+		}
 	}
 }
